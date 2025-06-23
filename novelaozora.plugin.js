1d3817965ab8fd517bfa9063219ef75a6058b679
Mete3586 <150524314+Mete3586@users.noreply.github.com>
2025-06-23 23:37:36 +0300

    Update novelaozora.plugin.js

--- novelaozora.plugin.js
+++ novelaozora.plugin.js
@@ -5,5 +5,40 @@ module.exports = {
   icon: '',
   language: ['tr'],
   site: 'https://novelaozora.com',
-  routes: [],
+  routes: [
+    {
+      path: '/seri/:slug/',
+      method: 'GET',
+      handler: async ({ slug }) => {
+        const page = await request.get(`https://novelaozora.com/seri/${slug}/`)
+        const $ = cheerio.load(page)
+
+        const chapters = []
+        $('.chapters li a').each((i, el) => {
+          chapters.push({
+            name: $(el).text().trim(),
+            url: $(el).attr('href'),
+          })
+        })
+
+        return {
+          name: slug.replace(/-/g, ' '),
+          chapters,
+        }
+      },
+    },
+    {
+      path: '/:chapterUrl',
+      method: 'GET',
+      handler: async ({ chapterUrl }) => {
+        const page = await request.get(`https://novelaozora.com/${chapterUrl}`)
+        const $ = cheerio.load(page)
+
+        const content = $('.text-left').html()
+        return {
+          content,
+        }
+      },
+    },
+  ],
 }
